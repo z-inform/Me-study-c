3f479ee2918d4ddd9fa0091e2072d5d6211d8018
z-inform <70823214+z-inform@users.noreply.github.com>
2020-09-06 01:35:01 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void solveLinear (double a, double b){ // ax + b = 0
+    printf("Solving linear equation: %gx + (%g) = 0\n", a, b);
+    if ( (a == 0) && (b == 0)) printf ("Infinite number of roots\n");
+
+    else if ( (a == 0) && (b != 0)) printf ("There are no roots\n");
+
+    else if ( (a != 0) && (b != 0))
+        {
+        double root = -b / a;
+        printf ("Root of the equation is: %g\n", root);
+        }
+    }
+
+void solveQuadratic (double a, double b, double c){ // ax^2 + bx + c = 0
+    double root1 = 0;
+    double root2 = 0;
+
+    printf("Solving quadratic equation: %gx^2 + %gx + %g = 0\n", a, b, c);
+
+    double dis = pow(b, 2) - 4*a*c;
+
+    if ( dis < 0 ) printf("Discriminant below zero, no roots\n");
+    else if ( dis == 0 ) {
+        root1 = -b/(2*a);
+        printf("Discriminant is zero, one root: %g\n", root1);
+        }
+    else {
+        root1 = (-b + sqrt(dis))/(2*a);
+        root2 = (-b - sqrt(dis))/(2*a);
+        printf("Discriminant more than zero, two roots: %g, %g\n", root1, root2);
+        }
+    }
+
+int main(){
+    double a = 0;// ax^2 + bx + c = 0
+    double b = 0;
+    double c = 0;
+
+    printf("Equation solver 1.0 by _: \"Solve your equation absolutely free\"\n"
+           "Enter coeffs. for your equation (ax^2 + bx + c = 0)\n");
+    printf("a = "); scanf("%lg", &a);
+    printf("b = "); scanf("%lg", &b);
+    printf("c = "); scanf("%lg", &c);
+    printf("\n");
+
+    if ( a == 0 ) solveLinear( b, c );
+    else solveQuadratic( a, b, c );
+
+    printf("\nYour equation is solved\n");
+
+    return 0;
+    }
+
