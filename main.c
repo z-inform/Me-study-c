7471d1ee3eb8aa94141597c25856ba37c6f802a2
Zakhar <z.inform@mail.ru>
2020-09-15 21:32:12 +0300

    Added input check

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int solveLinear(double a, double b, double* root);
 int solveQuadratic(double a, double b, double c, double* root1, double* root2);
 int testQuad(double a, double b, double c, int num, double root1, double root2);
 int testLin(double a, double b, int num, double root);
-
+double getnum();
 
 const double eps = 1e-7;
 const int noRoots = -1;
@@ -19,23 +19,28 @@ int main() {
     double c = 0;
 	double root1 = 0;
 	double root2 = 0;
-	
+
 	testQuad(3, 4, 2, noRoots, 0, 0);
 	testQuad(5, 8, -9, 2, 0.762049935, -2.362049935);
 	testQuad(1, 0, -4, 2, -2, 2);
 	testQuad(1, 6, 9, 1, -3, 0);
+	testQuad(1, 0, 0, 1, 0, 0);
 
 	testLin(0, 0, infRoots, 0);
 	testLin(0, 2, noRoots, 0);
-	testLin(-3, 9, 1, 3); 
-
+	testLin(-3, 9, 1, 3);
 
     printf("Equation solver 1.0 by Me: \"Solve your equation absolutely free\"\n"
            "Enter coeffs. for your equation (ax^2 + bx + c = 0)\n");
-    printf("a = "); scanf("%lg", &a);
-    printf("b = "); scanf("%lg", &b);
-    printf("c = "); scanf("%lg", &c);
-    printf("\n");
+
+	printf("a = ");
+	a = getnum();
+	
+	printf("b = ");
+	b = getnum();
+	
+	printf("c = ");
+	c = getnum();
 
     if ( isZero(a) ) solveLinear( b, c, &root1 );
     else solveQuadratic( a, b, c, &root1, &root2 );
@@ -65,6 +70,7 @@ int solveLinear (double a, double b, double* root) { // ax + b = 0
 
     else {
         *root = -b / a;
+		if (isZero(*root)) *root = 0;
         printf ("Root of the equation is: %g\n", *root);
 		return 1;
         }
@@ -82,12 +88,15 @@ int solveQuadratic (double a, double b, double c, double* root1, double* root2) 
 		}
     else if ( isZero(dis) ) {
         *root1 = -b/(2*a);
+		if (isZero(*root1)) *root1 = 0;
         printf("Discriminant is zero, one root: %g\n", *root1);
 		return 1;
         }
     else {
         *root1 = (-b + sqrt(dis))/(2*a);
+		if (isZero(*root1)) *root1 = 0;
         *root2 = (-b - sqrt(dis))/(2*a);
+		if (isZero(*root2)) *root2 = 0;
         printf("Discriminant more than zero, two roots: %g, %g\n", *root1, *root2);
 		return 2;
         }
@@ -97,15 +106,15 @@ int testQuad (double a, double b, double c, int num, double root1, double root2)
 	double gotRoot1 = 0;
 	double gotRoot2 = 0;
 	int res = solveQuadratic( a, b, c, &gotRoot1, &gotRoot2 );
-	if ( (res == num) && (isZero(root1 - gotRoot1) && isZero(root2 - gotRoot2)) ||
-			(isZero(root1 - gotRoot2) && isZero(root2 - gotRoot1))) { 
-		printf("Test %lg, %lg, %lg is SUCCESSFUL\n", a, b, c);
+	if ( (res == num) && ((isZero(root1 - gotRoot1) && isZero(root2 - gotRoot2)) ||
+			(isZero(root1 - gotRoot2) && isZero(root2 - gotRoot1)))) { 
+		printf("Test %lg, %lg, %lg is SUCCESSFUL\n\n", a, b, c);
 		return 0;
 		}
 		
 	else {
 		printf("Test %lg, %lg, %lg is FAILED. Expected num = %d, root1 = %lg, root2 = %lg. "
-		 	   "Got num = %d, root1 = %lg, root2 = %lg\n",
+		 	   "Got num = %d, root1 = %lg, root2 = %lg\n\n",
 			    a, b, c, num, root1, root2, res, gotRoot1, gotRoot2);	
 		return 1;
 		}
@@ -115,12 +124,12 @@ int testLin (double a, double b, int num, double root) {
 	double gotRoot = 0;
 	int res = solveLinear( a, b, &gotRoot);
 	if ( (res == num) && (isZero(root - gotRoot)) ) {
-			printf("Test %lg, %lg is SUCCESSFUL\n", a, b);
+			printf("Test %lg, %lg is SUCCESSFUL\n\n", a, b);
 			return 0;
 			}
 	else {
 		printf("Test %lg, %lg is FAILED. Expected num = %d, root = %lg. "
-		 	   "Got num = %d, root = %lg\n",
+		 	   "Got num = %d, root = %lg\n\n",
 			    a, b, num, root, res, gotRoot);
 		return 1;
 		}
@@ -128,7 +137,17 @@ int testLin (double a, double b, int num, double root) {
 }
 
 
-
+double getnum(){
+	double tnum = 0;
+	int res = scanf("%lg", &tnum);
+	while ( res == 0 ) {
+		while ( getchar() != '\n' ); 
+		printf("\nEnter correct number...\n");
+		res = scanf("%lg", &tnum);
+		}
+	return tnum;
+	
+}
 
 
 
