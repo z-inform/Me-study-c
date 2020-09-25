3d48e4d2b864fd582d96aed1e96559bb3a7378c0
Zakhar <z.inform@mail.ru>
2020-09-25 19:31:41 +0300

    strcat

---
+++ strfunc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* strcat(char* pstr1, char* pstr2);
+
+int main(){
+    char str1[10] = "abcdef";
+    char str2[10] = "xyz";
+    char* res = strcat(str1, str2);
+    for (int i = 0; res[i] != '\0'; i++) putchar(res[i]);
+    printf("\n");
+    return 0;
+}
+
+char* strcat(char* pstr1, char* pstr2){
+
+    int counter = 0;
+    char *presstr = (char*) malloc(sizeof(char));
+
+
+    for (; pstr1[counter] != '\0'; counter++){
+        presstr = (char*) realloc(presstr, sizeof(char));
+        presstr[counter] = pstr1[counter];
+    }
+
+    
+    for (int i = 0; pstr2[i] != '\0'; i++){
+        presstr = (char*) realloc(presstr, sizeof(char));
+        presstr[counter] = pstr2[i];
+        counter++;
+    }
+    
+return &presstr[0];
+}
+
