27bd94c6d505cdc5f3e5e9e26e1f798d6769d40f
Zakhar <z.inform@mail.ru>
2020-09-27 00:02:44 +0300

    Я познал еще часть пути истинного, и теперь код чуть меньше бред и чуть больше работает

--- strfunc.c
+++ strfunc.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
 
 char* strcat(char* pstr1, char* pstr2);
 
 int main(){
-    char str1[10] = "abcdef";
-    char str2[10] = "xyz";
-    char* res = strcat(str1, str2);
-    for (int i = 0; res[i] != '\0'; i++) putchar(res[i]);
+    char str1[500] = "LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOng";
+    char str2[100] = "NEWSTR";
+    char str3[10] = "    ";
+    char str4[2] = "";
+    char* res = strcat(str1, str3);
+    res = strcat(res, str2);
+    res = strcat(res, str4);
+    res = strcat(res, str2);
+    puts(res);
     printf("\n");
     return 0;
 }
@@ -15,21 +21,19 @@ int main(){
 char* strcat(char* pstr1, char* pstr2){
 
     int counter = 0;
-    char *presstr = (char*) calloc(1, sizeof(char));
+    char *presstr = (char*) calloc(strlen(pstr1) + strlen(pstr2) + 1, sizeof(char));
 
 
     for (; pstr1[counter] != '\0'; counter++){
-        presstr = (char*) realloc(presstr, counter*sizeof(char));
         presstr[counter] = pstr1[counter];
     }
 
     
     for (int i = 0; pstr2[i] != '\0'; i++){
-        ++counter;
-        presstr = (char*) realloc(presstr, counter*sizeof(char));
         presstr[counter] = pstr2[i];
+        counter++;
     }
     
-return presstr;
+    return presstr;
 }
 
