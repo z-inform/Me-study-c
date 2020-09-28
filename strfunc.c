eade0d467e86c6da08dfd185f8440f2ddfacfc88
Zakhar <z.inform@mail.ru>
2020-09-28 22:21:08 +0300

    strtok без правильных повторных вызовов

--- strfunc.c
+++ strfunc.c
@@ -3,16 +3,20 @@
 #include <cstring>
 
 char* strcat(char* pstr1, char* pstr2);
+char* strtok(char* str, char* del);
 
 int main(){
     char str1[500] = "LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOng";
     char str2[100] = "NEWSTR";
     char str3[10] = "    ";
     char str4[2] = "";
+    char str5[50] = "alylala, 15 - lastly";
+    char del[5] = ",-";
     char* res = strcat(str1, str3);
     res = strcat(res, str2);
     res = strcat(res, str4);
     res = strcat(res, str2);
+    res = strtok(str5, del);
     puts(res);
     printf("\n");
     return 0;
@@ -37,3 +41,21 @@ char* strcat(char* pstr1, char* pstr2){
     return presstr;
 }
 
+char* strtok(char* str, char* del){
+
+    char* lex = (char*) calloc(strlen(str) + 1, sizeof(char));
+
+    for(int i = 0; (str[i] != '\0'); i++){
+
+        for(int c = 0; del[c] != '\0'; c++){
+            if( str[i] == del[c]){
+                lex = (char*) realloc(lex, (strlen(lex) + 1)*sizeof(char));
+                return lex;
+            }
+            else lex[i] = str[i];
+        }
+
+    }
+    return lex;
+}
+    
